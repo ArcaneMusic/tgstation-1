ef261098eec22c3674ec8c535bea626ad2a086d0
ArcaneMusic <hero12290@aol.com>
2021-02-20 11:18:19 -0500

    tabs fixed

--- tgui/packages/tgui/interfaces/Techweb.js
+++ tgui/packages/tgui/interfaces/Techweb.js
@@ -155,61 +155,61 @@ export const TechwebContent = (props, context) => {
   ] = useLocalState(context, 'lastPoints', {});
 
   return (
-        <Flex direction="column" className="Techweb__Viewport" height="100%">
-          <Flex.Item className="Techweb__HeaderSection">
-            <Flex className="Techweb__HeaderContent">
-              <Flex.Item>
-                <Box>
-                  Available points:
-                  <ul className="Techweb__PointSummary">
-                    {Object.keys(points).map(k => (
-                      <li key={k}>
-                        <b>{k}</b>: {points[k]}
-                        {!!points_last_tick[k] && (
-                          ` (+${points_last_tick[k]}/sec)`
-                        )}
-                      </li>
-                    ))}
-                  </ul>
-                </Box>
-                <Box>
-                  Security protocols:
-                  <span
-                    className={`Techweb__SecProtocol ${!!sec_protocols && "engaged"}`}>
-                    {sec_protocols ? "Engaged" : "Disengaged"}
-                  </span>
-                </Box>
-              </Flex.Item>
-              <Flex.Item grow={1} />
+    <Flex direction="column" className="Techweb__Viewport" height="100%">
+      <Flex.Item className="Techweb__HeaderSection">
+        <Flex className="Techweb__HeaderContent">
+          <Flex.Item>
+            <Box>
+              Available points:
+              <ul className="Techweb__PointSummary">
+                {Object.keys(points).map(k => (
+                  <li key={k}>
+                    <b>{k}</b>: {points[k]}
+                    {!!points_last_tick[k] && (
+                      ` (+${points_last_tick[k]}/sec)`
+                    )}
+                  </li>
+                ))}
+              </ul>
+            </Box>
+            <Box>
+              Security protocols:
+              <span
+                className={`Techweb__SecProtocol ${!!sec_protocols && "engaged"}`}>
+                {sec_protocols ? "Engaged" : "Disengaged"}
+              </span>
+            </Box>
+          </Flex.Item>
+          <Flex.Item grow={1} />
+          <Flex.Item>
+            <Button fluid
+              onClick={() => act("toggleLock")}
+              icon="lock">
+              Lock Console
+            </Button>
+            {d_disk && (
               <Flex.Item>
                 <Button fluid
-                  onClick={() => act("toggleLock")}
-                  icon="lock">
-                  Lock Console
+                  onClick={() => setTechwebRoute({ route: "disk", diskType: "design" })}>
+                  Design Disk Inserted
                 </Button>
-                {d_disk && (
-                  <Flex.Item>
-                    <Button fluid
-                      onClick={() => setTechwebRoute({ route: "disk", diskType: "design" })}>
-                      Design Disk Inserted
-                    </Button>
-                  </Flex.Item>
-                )}
-                {t_disk && (
-                  <Flex.Item>
-                    <Button fluid
-                      onClick={() => setTechwebRoute({ route: "disk", diskType: "tech" })}>
-                      Tech Disk Inserted
-                    </Button>
-                  </Flex.Item>
-                )}
               </Flex.Item>
-            </Flex>
-          </Flex.Item>
-          <Flex.Item className="Techweb__RouterContent" height="100%">
-            <TechwebRouter />
+            )}
+            {t_disk && (
+              <Flex.Item>
+                <Button fluid
+                  onClick={() => setTechwebRoute({ route: "disk", diskType: "tech" })}>
+                  Tech Disk Inserted
+                </Button>
+              </Flex.Item>
+            )}
           </Flex.Item>
         </Flex>
+      </Flex.Item>
+      <Flex.Item className="Techweb__RouterContent" height="100%">
+        <TechwebRouter />
+      </Flex.Item>
+    </Flex>
   );
 };
 
